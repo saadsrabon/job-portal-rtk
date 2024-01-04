0407d7b9d969528331f5f18ae3757a1836c8d639
Saad Srabon <saadsrabon2@gmail.com>
2024-01-05 03:08:20 +0600

    thunk function added in reducers

--- src/redux/features/jobs/jobSlice.jsx
+++ src/redux/features/jobs/jobSlice.jsx
@@ -19,7 +19,30 @@ const jobSlice = createSlice({
         isLoading: false,
         error: null,
     },
-    extraReducers:{}
+    extraReducers:builder=>{
+        builder.addCase(fetchJobsThunk.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchJobsThunk.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.jobs = action.payload;
+        });
+        builder.addCase(fetchJobsThunk.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message;
+        });
+        builder.addCase(addJobsThunk.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(addJobsThunk.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.jobs.push(action.payload);
+        });
+        builder.addCase(addJobsThunk.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+    }
 });
 
 export default jobSlice.reducer;
