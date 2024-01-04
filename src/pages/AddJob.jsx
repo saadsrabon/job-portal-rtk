befc11c477bcad09888a5f887210385d95aa2776
Saad Srabon <saadsrabon2@gmail.com>
2024-01-05 02:37:56 +0600

    Add job function added in form

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -37,7 +37,7 @@ const AddJob = () => {
       <main className="max-w-3xl rounded-lg mx-auto relative z-20 p-10 xl:max-w-none bg-[#1E293B]">
         <h1 className="mb-10 text-center lws-section-title">Add New Job</h1>
 
-        <div className="max-w-3xl mx-auto">
+        <form onSubmit={handleAddJob} className="max-w-3xl mx-auto">
           <form className="space-y-6">
             <div className="fieldContainer">
               <label htmlFor="lws-JobTitle" className="text-sm font-medium text-slate-300">Job Title</label>
@@ -90,7 +90,7 @@ const AddJob = () => {
               </button>
             </div>
           </form>
-        </div>
+        </form>
       </main>
     </div>
     </>
