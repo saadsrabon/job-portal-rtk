9be511cf65950d3abc903388286542be6d23e299
Saad Srabon <saadsrabon2@gmail.com>
2024-01-05 03:35:46 +0600

    dynamic comment added

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -4,59 +4,59 @@ import { fetchJobsThunk } from "../redux/features/jobs/jobSlice"
 
 
 const Jobs = () => {
-   const jobs =useSelector((state)=>state?.job)
-console.log(jobs)
+   const {jobs,isLoading,error}=useSelector((state)=>state.job)
+
     const dispatch =useDispatch()
     useEffect(() => {
         dispatch(fetchJobsThunk())
     }, [dispatch])
 
     let content= null;
-    // if (isLoading) {
-    //     content = <div>Loading...</div>;
-    // }
-    // else if (error) {
-    //     content = <div>{error}</div>;
-    // }
-    // else {
+    if (isLoading) {
+        content = <div>Loading...</div>;
+    }
+    else if (error) {
+        content = <div>{error}</div>;
+    }
+    else {
 
-    //     jobs.length>0 && (content = jobs.map((job,index) => (
-    //         <div key={index} className="lws-single-job">
-    //         <div className="flex-1 min-w-0">
-    //             <h2 className="lws-title">{job?.jobTitle}</h2>
-    //             <div className="job-footers">
-    //                 <div className="lws-type">
-    //                     {/* <!-- Fulltime - #FF8A00,  --><!-- Internship - #FF5757,  --><!-- Remote - #56E5C4,  --> */}
-    //                     <i className="fa-solid fa-stop !text-[#FF8A00] text-lg mr-1.5"></i>
-    //                     {job?.jobType}
-    //                 </div>
-    //                 <div className="lws-salary">
-    //                     <i className="fa-solid fa-bangladeshi-taka-sign text-slate-400 text-lg mr-1.5"></i>
-    //                     BDT {job?.jobSalary}
-    //                 </div>
-    //                 <div className="lws-deadline">
-    //                     <i className="fa-regular fa-calendar text-slate-400 text-lg mr-1.5"></i>
-    //                     Closing on {job?.jobDeadline}
-    //                 </div>
-    //             </div>
-    //         </div>
-    //         <div className="mt-5 flex lg:mt-0 lg:ml-4">
-    //             <span className="hidden sm:block">
-    //                 <button type="button" className="lws-edit btn btn-primary">
-    //                     <i className="fa-solid fa-pen text-gray-300 -ml-1 mr-2"></i>
-    //                     Edit
-    //                 </button>
-    //             </span>
+        jobs.length>0 && (content = jobs.map((job,index) => (
+            <div key={index} className="lws-single-job">
+            <div className="flex-1 min-w-0">
+                <h2 className="lws-title">{job?.title}</h2>
+                <div className="job-footers">
+                    <div className="lws-type">
+                        {/* <!-- Fulltime - #FF8A00,  --><!-- Internship - #FF5757,  --><!-- Remote - #56E5C4,  --> */}
+                        <i className="fa-solid fa-stop !text-[#FF8A00] text-lg mr-1.5"></i>
+                        {job?.type}
+                    </div>
+                    <div className="lws-salary">
+                        <i className="fa-solid fa-bangladeshi-taka-sign text-slate-400 text-lg mr-1.5"></i>
+                        BDT {job?.salary}
+                    </div>
+                    <div className="lws-deadline">
+                        <i className="fa-regular fa-calendar text-slate-400 text-lg mr-1.5"></i>
+                        Closing on {job?.deadline}
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5 flex lg:mt-0 lg:ml-4">
+                <span className="hidden sm:block">
+                    <button type="button" className="lws-edit btn btn-primary">
+                        <i className="fa-solid fa-pen text-gray-300 -ml-1 mr-2"></i>
+                        Edit
+                    </button>
+                </span>
 
-    //             <span className="sm:ml-3">
-    //                 <button type="button" className="lws-delete btn btn-danger ">
-    //                     <i className="fa-solid fa-trash text-gray-300 -ml-1 mr-2"></i>
-    //                     Delete
-    //                 </button>
-    //             </span>
-    //         </div>
-    //     </div>)))
-    // }
+                <span className="sm:ml-3">
+                    <button type="button" className="lws-delete btn btn-danger ">
+                        <i className="fa-solid fa-trash text-gray-300 -ml-1 mr-2"></i>
+                        Delete
+                    </button>
+                </span>
+            </div>
+        </div>)))
+    }
 
   return (
     <div className="lg:pl-[14rem]  mt-[5.8125rem]">
