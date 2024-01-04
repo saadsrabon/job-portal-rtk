571b9474e33a068a82ec37291de03395ff5378ef
Saad Srabon <saadsrabon2@gmail.com>
2024-01-05 03:02:27 +0600

    add job function added

--- src/redux/features/jobs/jobsApi.jsx
+++ src/redux/features/jobs/jobsApi.jsx
@@ -4,3 +4,9 @@ export const fetchJobs =('jobs/fetchJobs', async () => {
   const response = axiosInstance.get('/jobs');
   return response.data
 });
+
+
+export const addJob =('jobs/addJob', async (payload) => {
+  const response = axiosInstance.post('/jobs', payload);
+  return response.data
+});
