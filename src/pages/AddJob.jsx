c37ac049a03bc1c3a2e2a7db373aff2d8bace5cf
Saad Srabon <saadsrabon2@gmail.com>
2024-01-05 02:28:06 +0600

    job title vallue added

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
 
-
+// create states for each input field
 const AddJob = () => {
+ const [jobTitle, setJobTitle] = useState('')
+  const [jobType, setJobType] = useState('')
+  const [jobSalary, setJobSalary] = useState('')
+  const [jobDeadline, setJobDeadline] = useState('')
+
+ const handleAddJob = (e) => {}
+
   return (
     <>
     <div className="lg:pl-[14rem] mt-[5.8125rem]">
@@ -12,21 +20,21 @@ const AddJob = () => {
             <div className="fieldContainer">
               <label htmlFor="lws-JobTitle" className="text-sm font-medium text-slate-300">Job Title</label>
               <select id="lws-JobTitle" name="lwsJobTitle" required>
-                <option value="" hidden selected>Select Job</option>
-                <option>Software Engineer</option>
-                <option>Software Developer</option>
-                <option>Full Stack Developer</option>
-                <option>MERN Stack Developer</option>
-                <option>DevOps Engineer</option>
-                <option>QA Engineer</option>
-                <option>Product Manager</option>
-                <option>Social Media Manager</option>
-                <option>Senior Executive</option>
-                <option>Junior Executive</option>
-                <option>Android App Developer</option>
-                <option>IOS App Developer</option>
-                <option>Frontend Developer</option>
-                <option>Frontend Engineer</option>
+                <option defaultValue>Select Job</option>
+                <option value="Software Developer">Software Developer</option>
+                <option value="Full Stack Developer">Full Stack Developer</option>
+                <option value="MERN Stack Developer">MERN Stack Developer</option>
+                <option value="DevOps Engineer">DevOps Engineer</option>
+                <option value="QA Engineer">QA Engineer</option>
+                <option value="Product Manager">Product Manager</option>
+                <option value="Social Media Manager">Social Media Manager</option>
+                <option value="Senior Executive">Senior Executive</option>
+                <option value="Junior Executive">Junior Executive</option>
+                <option value="Android App Developer">Android App Developer</option>
+                <option value="IOS App Developer">IOS App Developer</option>
+                <option value="Frontend Developer">Frontend Developer</option>
+                <option value="Frontend Engineer">Frontend Engineer</option>
+                <option value="Software Engineer" >Software Engineer</option>
               </select>
             </div>
 
